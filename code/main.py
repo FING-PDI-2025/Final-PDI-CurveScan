e8d302f53265ce020305bbf2453a1a972b3d84e3
Federico Williamson <federicowilliamson@hotmail.co.uk>
2025-05-26 20:16:34 -0300

    Test different morph patterns

--- code/main.py
+++ code/main.py
@@ -12,6 +12,7 @@ import pandas as pd
 import logging
 from rich.layout import Panel
 from rich.logging import RichHandler
+from rich.progress import track
 
 logging.basicConfig(
     level="NOTSET", format="%(message)s", datefmt="[%X]", handlers=[RichHandler()]
@@ -67,16 +68,16 @@ class Dataset:
         return df
 
     def get(
-        self, name: str, has_flash: bool = True, has_light: bool = True
+            self, name: str, has_flash: bool = True, has_light: bool = True
     ) -> Optional["DatasetItem"]:
         """
         Get the image with the given name and properties
         """
         for item in self.items:
             if (
-                item.name == name
-                and item.has_flash == has_flash
-                and item.has_light == has_light
+                    item.name == name
+                    and item.has_flash == has_flash
+                    and item.has_light == has_light
             ):
                 return item
         return None
@@ -187,15 +188,15 @@ class Utils:
         Show the image using OpenCV
         """
         for i, img in enumerate(image):
-            window = f"Image {i+offset}"
+            window = f"Image {i + offset}"
             cv.namedWindow(window, cv2.WINDOW_NORMAL)
             cv.imshow(window, img)
             pos = (i + offset)
             x = pos % 12
             y = pos // 12
-
-            cv.moveWindow(window, 600 * x + 1, 600 * y + 1)
-            cv.resizeWindow(window, 600, 600)
+            size = 900
+            cv.moveWindow(window, size * x + 1, size * y + 1)
+            cv.resizeWindow(window, size, size)
         if not wait:
             return
         cv.waitKey(0)
@@ -240,7 +241,7 @@ def region_detect(image: np.ndarray) -> np.ndarray:
     return image
 
 
-def region_detect2(image: np.ndarray) -> tuple[Img, float,  Contour, Contour]:
+def region_detect2(image: np.ndarray) -> tuple[Img, float, Contour, Contour]:
     """
     Detect the regions of the image based on Canny edges
     """
@@ -280,6 +281,7 @@ def is_image_blurry(image: np.ndarray) -> float:
     laplacian_var = cv.Laplacian(gray, cv.CV_64F).var()
     return laplacian_var
 
+
 def is_image_blurry2(image: np.ndarray) -> float:
     """
     Check if the image is blurry using the Canny edge detection method
@@ -288,6 +290,8 @@ def is_image_blurry2(image: np.ndarray) -> float:
     edges = cv.Canny(gray, 100, 200)
     edge_count = np.sum(edges > 0)
     return edge_count
+
+
 def is_image_blurry3(image: np.ndarray) -> float:
     """
     Check if the image is blurry using Fourier transform
@@ -299,6 +303,7 @@ def is_image_blurry3(image: np.ndarray) -> float:
     mean_magnitude = np.mean(magnitude_spectrum)
     return mean_magnitude
 
+
 def sharpen(image: np.ndarray) -> np.ndarray:
     """
     Sharpen the image using a kernel
@@ -306,6 +311,7 @@ def sharpen(image: np.ndarray) -> np.ndarray:
     kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
     sharpened = cv.filter2D(image, -1, kernel)
     return sharpened
+
 
 def low_pass_filter(image: np.ndarray) -> np.ndarray:
     """
@@ -315,6 +321,7 @@ def low_pass_filter(image: np.ndarray) -> np.ndarray:
     filtered = cv.filter2D(image, -1, kernel)
     return filtered
 
+
 def high_pass_filter(image: np.ndarray) -> np.ndarray:
     """
     Apply a high pass filter to the image
@@ -322,6 +329,7 @@ def high_pass_filter(image: np.ndarray) -> np.ndarray:
     kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
     filtered = cv.filter2D(image, -1, kernel)
     return filtered
+
 
 def find_contour(image: np.ndarray) -> tuple[np.ndarray, float]:
     """
@@ -339,38 +347,44 @@ def find_contour(image: np.ndarray) -> tuple[np.ndarray, float]:
     image = cv.bitwise_and(image, image, mask=mask)
     return image, area
 
+
 def main():
     dataset = Dataset()
-    render=False
-    hide_correct=False
+    render = False
+    hide_correct = False
+    wait_single = True
+    batch_size = 4
     # log.info(dataset.pretty_df)
-    sample = dataset.get("simple", False, False)
-    log.info(sample)
+    # sample = dataset.get("simple", False, False)
+    # log.info(sample)
     df_rows = []
     proced = process_samples(dataset)
-    for i, item in enumerate(itertools.batched((it for it in proced if not hide_correct or not it[1]['is_correct (estimated)']), 4)):
+    for i, item in enumerate(
+            itertools.batched((it for it in track(proced, total=len(dataset.items), console=console) if not hide_correct or not it[1]['is_correct (estimated)']),
+                              batch_size)):
         batch = [x for x, y in item if x is not None]
         df_rows.extend([y for x, y in item])
         if not render: continue
-        print([x.shape for x in batch])
         if len(batch) != 1:
             batch = np.vstack(batch)
         else:
             batch = batch[0]
-        Utils.show_image(batch, wait=False, offset=i)
+        Utils.show_image(batch, wait=wait_single, offset=0)
     df = pd.DataFrame(df_rows)
     # df = df.sort_values(["is_correct (estimated)", "blur", "name", "has_flash", "has_light"]).reset_index(drop=True)
-    console.print(Panel(str(df), highlight=True))
+    console.print(Panel(str(df), highlight=True, expand=False))
     console.print(f"Total correct detections (estimated): {df['is_correct (estimated)'].sum()}")
     # Is correct group by (has_flash and has_light)
-    correct_grouped = df.groupby(["has_flash", "has_light"])["is_correct (estimated)"].agg(['sum', 'count']).reset_index()
+    correct_grouped = df.groupby(["has_flash", "has_light"])["is_correct (estimated)"].agg(
+        ['sum', 'count']).reset_index()
     correct_grouped['percentage'] = (correct_grouped['sum'] / correct_grouped['count']) * 100
-    console.print(Panel(str(correct_grouped), highlight=True, title="Correct Detections Grouped by Flash and Light"))
+    console.print(Panel(str(correct_grouped), highlight=True, title="Correct Detections Grouped by Flash and Light",
+                        expand=False))
 
     correct_grouped = df.groupby(["name"])["is_correct (estimated)"].agg(
         ['sum', 'count']).reset_index()
     correct_grouped['percentage'] = (correct_grouped['sum'] / correct_grouped['count']) * 100
-    console.print(Panel(str(correct_grouped), highlight=True, title="Correct Detections Grouped by Name"))
+    console.print(Panel(str(correct_grouped), highlight=True, title="Correct Detections Grouped by Name", expand=False))
 
     correct_total = df['is_correct (estimated)'].sum()
     total_images = len(df)
@@ -378,7 +392,7 @@ def main():
         f"Total correct detections (estimated): {correct_total} out of {total_images} ({(correct_total / total_images) * 100:.2f}%)"
     )
 
-    if render: Utils.show_image(wait=True)
+    if render and not wait_single: Utils.show_image(wait=True)
 
 
 def process_samples(dataset):
@@ -386,49 +400,63 @@ def process_samples(dataset):
         yield from process_sample(smp)
 
 
+def analyse_results(processed, area, contour, clean_contour):
+    """
+    Analyse the results of the processed image
+    """
+    # Detect corners in the contour
+    approx = cv2.approxPolyN(clean_contour, 4)
+    corners = cv.drawContours(np.zeros_like(processed), [approx], -1, (255, 255, 255), thickness=cv.FILLED)
+
+    # Count number of non-black pixels in the processed image
+    non_black_corners = np.count_nonzero(corners)
+    non_black_processed = np.count_nonzero(processed)
+    size_ratio = abs(non_black_corners / (non_black_processed + 1) - 1)
+
+    is_correct = area > 100_000 and area < 1_000_000 and size_ratio < 0.1
+
+    return processed, area, contour, clean_contour, is_correct, size_ratio, corners
+
 def process_sample(smp):
     blurry = is_image_blurry3(smp.data)
     is_blurry = blurry < 50
     image_data = smp.data
-    raw_processed, raw_area, raw_contour, raw_clean_contour = region_detect2(image_data)
-    morph_processed, morph_area, morph_contour, morph_clean_contour = morphological_region_detect(image_data)
+    raw_processed, raw_area, raw_contour, raw_clean_contour, raw_is_correct, raw_size_ratio, raw_corners = analyse_results(*region_detect2(image_data))
+    morph_r_processed, morph_r_area, morph_r_contour, morph_r_clean_contour, morph_r_is_correct, morph_r_size_ratio, morph_r_corners = analyse_results(*morphological_region_detect(image_data, 2))
+    morph_g_processed, morph_g_area, morph_g_contour, morph_g_clean_contour, morph_g_is_correct, morph_g_size_ratio, morph_g_corners = analyse_results(*morphological_region_detect(image_data, 1))
+    morph_b_processed, morph_b_area, morph_b_contour, morph_b_clean_contour, morph_b_is_correct, morph_b_size_ratio, morph_b_corners = analyse_results(*morphological_region_detect(image_data, 0))
 
-    morph_detected_something = morph_area > 100_000
-    raw_detected_something = raw_area > 100_000
+    precedence = ["morph_b", "morph_g", "morph_r", "raw"]
+    processed = None
+    area = 0
+    contour = None
+    clean_contour = None
+    is_correct = False
+    size_ratio = 0
+    corners = None
+    # Select first that "is_correct"
+    for name in precedence:
+        if locals()[f"{name}_is_correct"]:
+            processed = locals()[f"{name}_processed"]
+            area = locals()[f"{name}_area"]
+            contour = locals()[f"{name}_contour"]
+            clean_contour = locals()[f"{name}_clean_contour"]
+            is_correct = locals()[f"{name}_is_correct"]
+            size_ratio = locals()[f"{name}_size_ratio"]
+            corners = locals()[f"{name}_corners"]
+            break
+    if processed is None:
+        # Show morph_b
+        processed = morph_b_processed
+        area = morph_b_area
+        contour = morph_b_contour
+        clean_contour = morph_b_clean_contour
+        is_correct = morph_b_is_correct
+        size_ratio = morph_b_size_ratio
+        corners = morph_b_corners
 
-
-    morph_approx = cv2.approxPolyN(morph_clean_contour, 4)
-    morph_corners = cv.drawContours(np.zeros_like(image_data), [morph_approx], -1, (255, 255, 255), thickness=cv.FILLED)
-
-    raw_approx = cv2.approxPolyN(raw_clean_contour, 4)
-    raw_corners = cv.drawContours(np.zeros_like(image_data), [raw_approx], -1, (255, 255, 255), thickness=cv.FILLED)
-
-    # count number of non-black pixels in the processed image
-    morph_non_black_corners = np.count_nonzero(morph_corners)
-    morph_non_black_processed = np.count_nonzero(morph_processed)
-    morph_size_ratio = abs(morph_non_black_corners / (morph_non_black_processed + 1) - 1)
-    morph_is_correct = morph_area > 100_000
-
-    raw_non_black_corners = np.count_nonzero(raw_corners)
-    raw_non_black_processed = np.count_nonzero(raw_processed)
-    raw_size_ratio = abs(raw_non_black_corners / (raw_non_black_processed + 1) - 1)
-    raw_is_correct = raw_area > 100_000 and raw_size_ratio < 0.1
-
-    if morph_is_correct:
-        processed = morph_processed
-        area = morph_area
-        contour = morph_contour
-        size_ratio = morph_size_ratio
-    else:
-        processed = raw_processed
-        area = raw_area
-        contour = raw_contour
-        size_ratio = raw_size_ratio
-
-
-    estimated_is_detection_correct = area > 100_000 and size_ratio < 0.1
     # log.debug(f"Morph: {morph_non_black_corners / (morph_non_black_processed + 1)}\n" + f"Raw: {raw_non_black_corners / (raw_non_black_processed + 1)}")
-    display = np.hstack((image_data, raw_processed, raw_corners, morph_processed, morph_corners))
+    display = np.hstack((image_data, processed, corners))
     # log.debug(display.shape)
 
     df_row = {
@@ -438,11 +466,7 @@ def process_sample(smp):
         "blur": blurry,
         "is_blurry": is_blurry,
         "area": area,
-        "raw_ratio": f"{raw_size_ratio:.2f}",
-        "raw_detected": raw_detected_something,
-        "morph_ratio": f"{morph_size_ratio:.2f}",
-        "morph_detected": morph_detected_something,
-        "is_correct (estimated)": estimated_is_detection_correct,
+        "is_correct (estimated)": is_correct,
     }
     # Ignore horizontal images.
     yield display if display.shape[0] > 899 else None, df_row
@@ -461,23 +485,27 @@ def process_sample(smp):
     # )
 
 
-def morphological_region_detect(image_data: np.ndarray) -> tuple[Img, float, Contour, Contour]:
+def morphological_region_detect(image_data: np.ndarray, color: int = 0) -> tuple[Img, float, Contour, Contour]:
     morphological_gradient: np.ndarray = cv.morphologyEx(image_data, cv.MORPH_GRADIENT, cv.getStructuringElement(cv.MORPH_RECT, (3, 3)))
+    yuv = cv.cvtColor(morphological_gradient, cv.COLOR_BGR2YUV)
+    yuv[:,:,0] = cv.equalizeHist(yuv[:,:,0])
+    eq = cv.cvtColor(yuv, cv.COLOR_YUV2BGR)
+    # Utils.show_image(eq)
     signed_mg: np.ndarray = morphological_gradient.copy().astype(np.int16)
-    mask: np.ndarray = mask_blue(signed_mg)
+    mask: np.ndarray = mask_blue(signed_mg, color)
     # Find contours in the morphological gradient
     contours, _ = cv.findContours(cv.cvtColor(mask, cv.COLOR_BGR2GRAY), cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
     if not contours:
         log.warning("No contours found in the morphological gradient.")
-        return 0, [], np.zeros_like(image_data)
+        return mask, 0, [], np.zeros_like(image_data)
     largest_contour = max(contours, key=cv.contourArea)
     # Draw the largest contour on the original image
     contour_render = cv.drawContours(np.zeros_like(mask), [largest_contour], -1, (255, 255, 255), thickness=cv.FILLED)
     contour_render = cv.cvtColor(contour_render, cv.COLOR_BGR2GRAY)
     contour_render = cv.medianBlur(contour_render, 5)
     # Apply morphological operations to clean up the contours
-    contour_render = cv.morphologyEx(contour_render, cv.MORPH_ERODE, cv.getStructuringElement(cv.MORPH_ELLIPSE, (50, 50)))
-    contour_render = cv.morphologyEx(contour_render, cv.MORPH_DILATE, cv.getStructuringElement(cv.MORPH_ELLIPSE, (50, 50)))
+    contour_render = cv.morphologyEx(contour_render, cv.MORPH_OPEN, cv.getStructuringElement(cv.MORPH_ELLIPSE, (50, 50)))
+    # contour_render = cv.morphologyEx(contour_render, cv.MORPH_DILATE, cv.getStructuringElement(cv.MORPH_ELLIPSE, (50, 50)))
     area = cv.contourArea(largest_contour)
     masked_image = cv.bitwise_and(image_data, image_data, mask=contour_render)
 
@@ -493,9 +521,13 @@ def morphological_region_detect(image_data: np.ndarray) -> tuple[Img, float, Con
     return masked_image, area, largest_contour, largest_clean_contour
 
 
-def mask_blue(inp: np.ndarray) -> np.ndarray:
+def mask_blue(inp: np.ndarray, color: int = 0) -> np.ndarray:
     # Detect "Blue"
-    mask: np.ndarray = 2 * inp[:, :, 0] - inp[:, :, 1] - inp[:, :, 2]
+    match color:
+        case 0: mask = 2 * inp[:, :, 0] - inp[:, :, 1] - inp[:, :, 2]
+        case 1: mask = 2 * inp[:, :, 1] - inp[:, :, 0] - inp[:, :, 2]
+        case 2: mask = 2 * inp[:, :, 2] - inp[:, :, 0] - inp[:, :, 1]
+
     mask[mask < 0] = 0
     mask = mask.astype(np.uint8)
     mask = cv.medianBlur(mask, 5)
